dd5284f4210199d9f43e4613f0dc62a6d4b44fef
Luke Sullivan <sully097@gmail.com>
2024-05-31 15:40:07 +0100

    Somewhat working

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,8 +1,29 @@
 def caesar_cipher(str, shift)
-  # alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
-  shift_arr = str.bytes.map {|i| i += shift}
+  # Keep track of case of each letter in string.
+  str_case = str.split('').map { |ltr| ltr == ltr.upcase }
+
+  # Create an array holding index values of the shifted characters.
+  shift_arr = str.downcase.bytes.map { |i| i + shift - 96 }
+
+  shift_arr = shift_arr.map do |i|
+    if i < 1
+      i + 122 
+    elsif i > 26
+      i + 70
+    else
+      i + 96
+    end
+  end
+
+  shift_str = shift_arr.pack('c*')
+
+  for i in str_case do
+    puts shift_str[i]
+  end
+  
+  shift_str
 end
 
-string = 'abc'
+string = 'Az'
 
-p caesar_cipher(string, 3)
+p caesar_cipher(string, -1)
