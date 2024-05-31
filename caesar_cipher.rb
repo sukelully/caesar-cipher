6f6ea3a6ed047cef31da0f98c5fd7aa9709b6f8c
Luke Sullivan <sully097@gmail.com>
2024-05-31 14:42:32 +0100

    Refactor

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,16 +1,6 @@
 def caesar_cipher(str, shift)
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
-  index_arr = []
-
-  str.split('').each do |letter|
-    index_arr << alphabet.index(letter)
-  end
-
-  shift_index_arr = index_arr.map do |i|
-    i += shift
-  end
-  
-  shift_index_arr
+  # alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  shift_arr = str.bytes.map {|i| i += shift}
 end
 
 string = 'abc'
