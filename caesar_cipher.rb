11d3dc5e9fe006720177baf0a8dbb5affb7cb2f8
Luke Sullivan <sully097@gmail.com>
2024-06-01 08:38:18 +0100

    Brickwall

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -5,25 +5,32 @@ def caesar_cipher(str, shift)
   # Create an array holding index values of the shifted characters.
   shift_arr = str.downcase.bytes.map { |i| i + shift - 96 }
 
+  # Adjust character byte value if lower than 1 or higher than 26.
   shift_arr = shift_arr.map do |i|
     if i < 1
       i + 122 
     elsif i > 26
       i + 70
+    elsif i == "\n"
+      next
     else
       i + 96
     end
   end
 
+  # Convert back to character.
   shift_str = shift_arr.pack('c*')
 
-  for i in str_case do
-    puts shift_str[i]
+  # 
+  str_case.each_with_index do |is_upper, index|
+    if is_upper
+      shift_str[index] = shift_str[index].upcase
+    end
   end
-  
+
   shift_str
 end
 
-string = 'Az'
+string = 'What a string!'
 
-p caesar_cipher(string, -1)
+p caesar_cipher(string, 5)
