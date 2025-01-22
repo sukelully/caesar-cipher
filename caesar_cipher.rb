27eeee1a73d301eacffc64c9e3bf6dd4acc5e64b
Luke Sullivan <sully097@gmail.com>
2025-01-22 21:24:27 +0000

    Complete cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,36 +1,17 @@
 def caesar_cipher(str, shift)
-  # Keep track of case of each letter in string.
-  str_case = str.split('').map { |ltr| ltr == ltr.upcase }
-
-  # Create an array holding index values of the shifted characters.
-  shift_arr = str.downcase.bytes.map { |i| i + shift - 96 }
-
-  # Adjust character byte value if lower than 1 or higher than 26.
-  shift_arr = shift_arr.map do |i|
-    if i < 1
-      i + 122 
-    elsif i > 26
-      i + 70
-    elsif i == "\n"
-      next
+  shift = shift % 26
+  shifted_str = str.chars.map do |char|
+    if char.match(/[a-z]/)
+      ((char.ord - 'a'.ord + shift) % 26 + 'a'.ord).chr
+    elsif char.match(/[A-Z]/)
+      ((char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
     else
-      i + 96
+      char
     end
   end
-
-  # Convert back to character.
-  shift_str = shift_arr.pack('c*')
-
-  # 
-  str_case.each_with_index do |is_upper, index|
-    if is_upper
-      shift_str[index] = shift_str[index].upcase
-    end
-  end
-
-  shift_str
+  shifted_str.join
 end
 
 string = 'What a string!'
 
-p caesar_cipher(string, 5)
+puts caesar_cipher(string, 7)
--- tempCodeRunnerFile.rb
+++
@@ -1,8 +0,0 @@
-  # for i in str_case do
-  #   # if i
-  #   #   # shift_str[i] = shift_str[i].upcase
-  #   #   p shift_str[i]
-  #   # end
-  #   puts shift_str[i]
-  # end
-  
